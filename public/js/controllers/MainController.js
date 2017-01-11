5212556ab49ff6248519c2e25a24aea8bc1a749d
Warenga <warenga.maina@andela.com>
2017-01-11 22:42:42 +0300

    Add localstorage and getIndex functionality

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,79 +1,88 @@
- app.controller('MainController', ['$scope', 'fileUpload', '$http', function($scope, fileUpload, $http) {
+ /*jshint esversion: 6 */
 
-   $scope.checked = [];
+ app.controller('MainController', ['$scope', 'fileUpload', '$http', '$localStorage',
+   function($scope, fileUpload, $http, $localStorage) {
 
-   $scope.toogleSelection = function toogleSelection(file) {
-     var ino = $scope.checked.indexOf(file);
 
-     if (ino > -1) {
-       $scope.checked.splice(ino, 1);
-     } else {
-       $scope.checked.push(file);
-     }
+     // Array that holds the checked files
+     $scope.checked = [];
 
-     console.log($scope.checked);
-   };
 
-   $scope.uploadFile = function() {
-     var file = $scope.myFile;
-     var uploadUrl = "/savedata";
-     fileUpload.uploadFileToUrl(file, uploadUrl);
-   };
+     $scope.fetchIndex = function(file) {
+       $scope.fileIndex = $localStorage[file];
+       return file, $scope.fileIndex;
+     };
 
-   $scope.getIndex = function() {
-     $http.post('/api/getIndex', $scope.checked).success(function(data, status, headers, config) {
-       $scope.words = data;
-       console.log($scope.words);
-     }).
-     error(function(data, status, header, config) {
-       $scope.ResponseDetails = "Data: " + data +
-         "<br />status: " + status +
-         "<br />headers: " + jsonFilter(header) +
-         "<br />config: " + jsonFilter(config);
-     });
-   };
+     // 
+     $scope.toogleSelection = function toogleSelection(file) {
+       var ino = $scope.checked.indexOf(file);
+       if (ino > -1) {
+         $scope.checked.splice(ino, 1);
+       } else {
+         $scope.checked.push(file);
+       }
+     };
 
-   $scope.searchIndex = function() {
-     console.log($scope.formData);
-     $http.post('/api/searchIndex', $scope.checked, $scope.formData).success(function(data) {
-       $scope.formData = {};
-       $scope.results = data;
-       console.log(data);
-     }).error(function(data) {
-       alert('error');
-     });
+     $scope.uploadFile = function() {
+       var file = $scope.myFile;
+       var uploadUrl = "/savedata";
+       fileUpload.uploadFileToUrl(file, uploadUrl).then(function(response) {
+         let fileName = file.name;
+         $localStorage[fileName] = response.data;
+       });
 
-   };
+     };
 
-   $scope.showFiles = function() {
-     $http.get('/api/files').success(function(
-       data) {
-       $scope.files = data;
-     }).error(function(data, status, headers, config) {
-       console.log(data, status, headers, config);
-     });
-   };
+     $scope.searchIndex = function() {
+       if ($scope.checked === []) {
+         $scope.files.forEach = function(file) {
+           let data = [file, $localStorage[file], $scope.formData];
+           $http.post('/api/searchIndex', data).success(function(data) {
+             $scope.formData = {};
+             $scope.searchedWords = data;
+           }).error(function(data) {
+             alert('error');
+           });
+         };
+       } else {
+         $scope.checked.forEach(function(file) {
+           let data = [file, $localStorage[file], $scope.formData];
+           $http.post('/api/searchIndex', data).success(function(data) {
+             $scope.formData = {};
+             $scope.searchedWords = data;
+           }).error(function(data) {
+             alert('error');
+           });
+         });
+       }
+     };
 
- }]);
+     $scope.showFiles = function() {
+       $http.get('/api/files').success(function(
+         data) {
+         $scope.files = data;
+       }).error(function(data, status, headers, config) {
+         console.log(data, status, headers, config);
+       });
+     };
+
+   }
+ ]);
 
  app.service('fileUpload', ['$http', function($http) {
    this.uploadFileToUrl = function(file, uploadUrl) {
      var fd = new FormData();
      fd.append('file', file);
 
-
-
-     $http.post(uploadUrl, fd, {
+     var promise = $http.post(uploadUrl, fd, {
        transformRequest: angular.identity,
        headers: {
          'Content-Type': undefined
        }
-     })
-
-     .success(function(data) {
+     }).success(function(data) {
        return data;
-     })
+     }).error(function() {});
 
-     .error(function() {});
+     return promise;
    };
  }]);
