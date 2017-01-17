0c109566b79cc550090ce018a3501e5c22c051d9
Warenga <warenga.maina@andela.com>
2017-01-17 20:44:48 +0300

    Change gulp jasmine to gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const jasmineNode = require('gulp-jasmine-node');
 const livereload = require('gulp-livereload');
 
 // Loads Jasmine Browser
-gulp.task('jasmine', function() {
+gulp.task('test', function() {
   return gulp.src("jasmine/spec/*_spec.js").pipe(jasmineNode());
 });
 
@@ -19,7 +19,7 @@ gulp.task('browser', function() {
 gulp.task('watch', function() {
   livereload.listen();
   // Checks for change in the jasmine folder
-  gulp.watch('jasmine/**/*.js', ['jasmine']);
+  gulp.watch('jasmine/**/*.js', ['test']);
 
   //Checks for change in all html file in public
   gulp.watch('public/*.html', ['browser']);
@@ -31,4 +31,4 @@ gulp.task('watch', function() {
   gulp.watch('public/js/*.js', ['browser']);
 });
 
-gulp.task('default', ['watch', 'browser', 'jasmine']);
+gulp.task('default', ['watch', 'browser', 'test']);
