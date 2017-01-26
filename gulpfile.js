30a5d0eb9aee2a987395516e2d3dc3ef26521330
Warenga <warenga.maina@andela.com>
2017-01-26 07:08:26 +0300

    (chore): Add comments for better documentation

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,23 @@
 
  gulp.task('default', ['browser-sync', 'test', 'watch']);
 
- // Loads Jasmine Browser
+ // Runs jasmine spec tests
  gulp.task('test', () => {
    gulp.src('jasmine/spec/*_spec.js').pipe(jasmineNode());
  });
 
+ // Loads jasmine browser 
  gulp.task('spec', () => {
    gulp.src('jasmine/specRunner.html').pipe(open());
  });
 
+// Watch jasmine spec file and calls spec if any change is detected
  gulp.task('watch', () => {
    gulp.watch('jasmine/spec/*_spec.js', ['spec']);
  });
 
+// Initializws browser sync and sets it to watch public files
+// if change is detected, nodemon is called.
  gulp.task('browser-sync', ['nodemon'], () => {
    browserSync.init(null, {
      proxy: 'http://localhost:1337',
